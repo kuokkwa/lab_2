7a08def0c6f74301d5a4b571f17becfe0d6667bf
kuokkwa <margaritaworkmail@gmail.com>
2023-09-14 11:02:02 +0300

    smth

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Address { 
+    public int index;
+    public string country;
+    public string city;
+    public string street;
+    public string house;
+    public string apartment;
+
+    public Address() {
+        index = 11111;
+        country = "Ukraine";
+        city = "Kyiv";
+        street = "Khreschatyk";
+        house = "none";
+        apartment = "a123";
+    }
+
+    public void Print() {
+        Console.WriteLine($"Index:{index}, Country:{country}, City:{city}, Street:{street}, House:{house}, Apartment:{apartment}");
+    }
+}
+class Converter
+{ double usd
+        double eur
+        double pln}
+class Employee
+{ }
+class User
+{ }
