2766ef96a591e4e00f575c10e3f19d6a18f3cc26
kuokkwa <margaritaworkmail@gmail.com>
2023-09-14 11:38:03 +0300

    smth

--- Program.cs
+++ Program.cs
@@ -1,29 +1,33 @@
-﻿class Address { 
-    public int index;
-    public string country;
-    public string city;
-    public string street;
-    public string house;
-    public string apartment;
+﻿using System;
 
-    public Address() {
-        index = 11111;
-        country = "Ukraine";
-        city = "Kyiv";
-        street = "Khreschatyk";
-        house = "none";
-        apartment = "a123";
-    }
+namespace Classes {
+    class Address {
+        public int index;
+        public string country;
+        public string city;
+        public string street;
+        public string house;
+        public string apartment;
 
-    public void Print() {
-        Console.WriteLine($"Index:{index}, Country:{country}, City:{city}, Street:{street}, House:{house}, Apartment:{apartment}");
+        public Address() {
+            index = 11111;
+            country = "Ukraine";
+            city = "Kyiv";
+            street = "Khreschatyk";
+            house = "none";
+            apartment = "a123";
+        }
+
+        public void Print() {
+            Console.WriteLine($"Index:{index}, Country:{country}, City:{city}, Street:{street}, House:{house}, Apartment:{apartment}");
+        }
     }
+    class Converter
+    { double usd
+            double eur
+            double pln}
+    class Employee
+    { }
+    class User
+    { }
 }
-class Converter
-{ double usd
-        double eur
-        double pln}
-class Employee
-{ }
-class User
-{ }
