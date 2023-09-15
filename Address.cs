8a7f64b870dbec23fe8b166a2c528c35ba34e2af
kuokkwa <margaritaworkmail@gmail.com>
2023-09-15 18:36:41 +0300

    smth

--- Address.cs
+++ Address.cs
@@ -1,19 +1,8 @@
-﻿namespace lab_2
+﻿using System;
+
+public class Class1
 {
-    internal class Address
-    {
-        public class Credentials
-        {
-            public int index = 11111;
-            public string country = "Ukraine";
-            public string city = "Kyiv";
-            public string street = "Khreshchatyk";
-            public string house = "A1";
-            public string apartment = "a123";
-            static void Main(string[] args)
-            {
-                Console.WriteLine(value: $"Index:{index}, Country:{country}, City:{city}, Street:{street}, House:{house}, apartment:{apartment}");
-            }
-        }
-    }
+	public Class1()
+	{
+	}
 }
