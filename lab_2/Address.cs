c4c0e4e3431428a15602cd0070ecd5c0841dee5f
kuokkwa <margaritaworkmail@gmail.com>
2023-09-15 18:38:24 +0300

    smth

--- lab_2/Address.cs
+++ lab_2/Address.cs
@@ -8,5 +8,18 @@ namespace lab_2
 {
     internal class Address
     {
+        public class Credentials
+        {
+            public int index = 11111;
+            public string country = "Ukraine";
+            public string city = "Kyiv";
+            public string street = "Khreshchatyk";
+            public string house = "A1";
+            public string apartment = "a123";
+            static void Main(string[] args)
+            {
+                Console.WriteLine(value: $"Index:{index}, Country:{country}, City:{city}, Street:{street}, House:{house}, apartment:{apartment}");
+            }
+        }
     }
 }
