26470e8c8a7589c08a843a0c1b26c84e3f3ea3e2
kuokkwa <margaritaworkmail@gmail.com>
2023-09-21 12:34:59 +0300

    c

--- lab_2/Address.cs
+++ lab_2/Address.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task_2
+namespace lab_2
 {
-    internal class Converter
+    internal class Address
     {
     }
 }
