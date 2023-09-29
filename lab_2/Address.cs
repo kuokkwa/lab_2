aaafd97e55bb1bbd48e8e7558f2d2ca6874340fe
kuokkwa <margaritaworkmail@gmail.com>
2023-09-29 16:09:41 +0300

    c

--- lab_2/Address.cs
+++ lab_2/Address.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace lab_2
 {
-    internal class Address
+    public class Address
     {
+        private int index;
+
+        private void getindex()
+        {
+            index = 11111;
+        }
+
+        public int Index()
+        {
+            return index;
+        }
+
+        private string country;
+
+        private void getcountry()
+        {
+            country = "Ukraine";
+        }
+
+        public string Country()
+        {
+            return country;
+        }
+
+        private string city;
+
+        private void getcity()
+        {
+            city = "Kyiv";
+        }
+
+        public string City()
+        {
+            return city;
+        }
+
+        private string street;
+
+        private void getstreet()
+        {
+            street = "Khreshchatyk";
+        } 
+
+        public string Street()
+        {
+            return street;
+        }
+
+        private string house;
+
+        private void gethouse()
+        {
+            house = "none";
+        }
+
+        public string House()
+        {
+            return house;
+        }
+
+        private int apartment;
+
+        private void getapartment()
+        {
+            apartment = 11;
+        }
+
+        public int Apartment()
+        {
+            return apartment;
+        }
     }
 }
--- lab_2/Program.cs
+++ lab_2/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Address address = new Address();
+            int index = address.Index();
+            string country = address.Country();
+            string city = address.City();
+            string street = address.Street();
+            string house = address.House();
+            int apartment = address.Apartment();
+            Console.WriteLine(index);
+            Console.WriteLine(country);
+            Console.WriteLine(city);
+            Console.WriteLine(street);
+            Console.WriteLine(house);
+            Console.WriteLine(apartment);
+
         }
     }
 }
