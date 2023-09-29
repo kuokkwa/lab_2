aac26267200a90228c9a749283f6315f612bb6ec
kuokkwa <margaritaworkmail@gmail.com>
2023-09-29 16:31:04 +0300

    09.29 (16:30)

--- lab_2/Address.cs
+++ lab_2/Address.cs
@@ -8,76 +8,12 @@ namespace lab_2
 {
     public class Address
     {
-        private int index;
+        public string Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment {  get; set; }
 
-        private void getindex()
-        {
-            index = 11111;
-        }
-
-        public int Index()
-        {
-            return index;
-        }
-
-        private string country;
-
-        private void getcountry()
-        {
-            country = "Ukraine";
-        }
-
-        public string Country()
-        {
-            return country;
-        }
-
-        private string city;
-
-        private void getcity()
-        {
-            city = "Kyiv";
-        }
-
-        public string City()
-        {
-            return city;
-        }
-
-        private string street;
-
-        private void getstreet()
-        {
-            street = "Khreshchatyk";
-        } 
-
-        public string Street()
-        {
-            return street;
-        }
-
-        private string house;
-
-        private void gethouse()
-        {
-            house = "none";
-        }
-
-        public string House()
-        {
-            return house;
-        }
-
-        private int apartment;
-
-        private void getapartment()
-        {
-            apartment = 11;
-        }
-
-        public int Apartment()
-        {
-            return apartment;
-        }
     }
 }
--- lab_2/Program.cs
+++ lab_2/Program.cs
@@ -5,18 +5,18 @@
         static void Main(string[] args)
         {
             Address address = new Address();
-            int index = address.Index();
-            string country = address.Country();
-            string city = address.City();
-            string street = address.Street();
-            string house = address.House();
-            int apartment = address.Apartment();
-            Console.WriteLine(index);
-            Console.WriteLine(country);
-            Console.WriteLine(city);
-            Console.WriteLine(street);
-            Console.WriteLine(house);
-            Console.WriteLine(apartment);
+            address.Index = "Index: 11111";
+            address.Country = "Country: Ukraine";
+            address.City = "City: Kyiv";
+            address.Street = "Street: Khreshchatyk";
+            address.House = "House: none";
+            address.Apartment = "Apartment: 11";
+            Console.WriteLine(address.Index);
+            Console.WriteLine(address.Country);
+            Console.WriteLine(address.City);
+            Console.WriteLine(address.Street);
+            Console.WriteLine(address.House);
+            Console.WriteLine(address.Apartment);
 
         }
     }
